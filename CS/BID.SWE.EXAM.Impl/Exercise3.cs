930d0e7ce4e2b8404dcb56cece2b64afce18a281
Theresa Lachtner <theresa.l1996@gmail.com>
2019-12-12 17:05:43 +0100

    ex 3

--- CS/BID.SWE.EXAM.Impl/Exercise3.cs
+++ CS/BID.SWE.EXAM.Impl/Exercise3.cs
@@ -12,8 +12,36 @@ namespace BID.SWE.EXAM.Impl
 
         public object Method2()
         {
-            throw new NotImplementedException();
+            Bewegung[] _bewegung = new Bewegung[2];
+            _bewegung[0] = new Springen();
+            _bewegung[1] = new Laufen();
+            return _bewegung;
         }
+    }
 
+    public class Bewegung
+    {
+        public virtual string Move (int x)
+        {
+            return "";
+        }
+        public virtual string Position()
+        {
+            return "";
+        }
+    }
+    public class Springen : Bewegung
+    {
+        public override string Move(int x)
+        {
+            return "x";
+        }
+    }
+    public class Laufen : Bewegung
+    {
+        public override string Position()
+        {
+            return "y";
+        }
     }
 }
--- CS/BID.SWE.EXAM.Impl/Exercise4.cs
+++ CS/BID.SWE.EXAM.Impl/Exercise4.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BID.SWE1.Exam.Interfaces;
+using System.IO;
 
 namespace BID.SWE.EXAM.Impl
 {
@@ -7,7 +8,11 @@ namespace BID.SWE.EXAM.Impl
     {
         public object Method1()
         {
+            string christmas = "Frohe Weihnachten";
+            int num = 42;
+            bool b = false;
             throw new NotImplementedException();
+
         }
 
         public object Method2(object obj)
